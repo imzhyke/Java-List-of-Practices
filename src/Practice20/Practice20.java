60f873dc54fef024f184eca82037bcd33a41aa93
imzhyke <75247669+imzhyke@users.noreply.github.com>
2021-04-14 09:47:57 +0800

    Comments Added

--- src/Practice20/Practice20.java
+++ src/Practice20/Practice20.java
@@ -1,34 +1,37 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ * Title: Practice 20
+ * Name: Ezekiel P. Villadolid
+ * Description: program that prompts the user to input an integer between 0 and 35. If the number is
+                less than or equal to 9, the program should output the number; otherwise, it should output A
+                for 10, B for 11, C for 12,....., and Z for 35.
+ * Date: 4/14/2021
+*/
 package Practice20;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Zhyke
- */
+
 public class Practice20 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+   public static void main(String[] args) {
+       //CREATING SCANNER
         Scanner console = new Scanner(System.in);
         
+        //ASK USER TO INPUT AN INTEGER VALUE 
         System.out.print("Enter numbers between 0-35: ");
         int num = console.nextInt();
         
-        if(num<=9||num>=0){
-            System.out.println(num);
+        //condition if a value is less than or equals to 9 and not lower than zero
+        if(num<=9&&num>=0){
+            System.out.println(num); //This will print the integer entered 
         }
-        else if(num>9||num<=35){
-            num+=55;
-            char c=(char)num;  
-            System.out.print(c);
+        //condition if a value is greater than to the value of 9 and not higher than 35
+        else if(num>9&&num<=35){
+            num=num+55; //Since character 'A' has the value of 65
+            char c=(char)num;  //To typecasting the value of num
+            System.out.println(c+" for "+(num-55));
         }
-    }
+        
+     }
+    
 }
