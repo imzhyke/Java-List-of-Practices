ce2b03ead41b521ed045e452b91b703a1037cfc1
imzhyke <75247669+imzhyke@users.noreply.github.com>
2021-04-19 22:08:09 +0800

    Added practice 23 and 24

---
+++ src/Practice23/Practice23.java
@@ -0,0 +1,8 @@
+package Practice23;
+
+public class Practice23 {
+    public static void main(String[] args) {
+        
+
+    }
+}
---
+++ src/Practice24/Practice24.java
@@ -0,0 +1,9 @@
+package Practice24;
+
+public class Practice24 {
+    public static void main(String[] args) {
+        
+    
+        
+    }
+}
