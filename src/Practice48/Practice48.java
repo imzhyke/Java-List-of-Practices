69c28948533c08e1b48486c8230b585437fce7a7
imzhyke <punk1313444@gmail.com>
2021-06-08 00:46:53 +0800

    New Practcice Added

---
+++ src/Practice48/Practice48.java
@@ -0,0 +1,68 @@
+/*
+ * Title : Practice 48
+ * Name : Ezekiel P. Villadolid
+ * Description: Bubble sort
+ * Date: 6/3/2021
+*/
+package Practice48;
+
+import java.util.Scanner;
+
+public class Practice48 {
+    
+        //The bubble sort method
+        static void bubbleSort(int[] arr) {  
+        int n = arr.length;  
+        int temp = 0;  
+        //Bubble sort process
+         System.out.println("Bubble Sort Process");  
+        for (int i = 0; i < ( n - 1 ); i++) {
+                for (int j = 0; j < n - i - 1; j++) {
+                        if (arr[j] < arr[j+1]){ 
+                                 //swap elements  
+                                temp = arr[j];
+                                arr[j] = arr[j+1];
+                                arr[j+1] = temp; 
+                         }  
+                          
+                 }  
+                //Print the elemets ever pass
+                for(int x=0; x < arr.length; x++){  
+                        System.out.print(arr[x] + " ");  
+                }  
+                System.out.println();  
+         }  
+  
+    }  
+    public static void main(String[] args) {  
+               //Creating scanner
+               Scanner thisScans = new Scanner(System.in);
+                 
+               //Get the size of array
+                System.out.print("Enter array Size: ");
+                int n = thisScans.nextInt();
+                
+                //Get the elements of an array
+                System.out.print("Enter elements of array: ");
+                int arr[] =new int [n];  
+                for(int x = 0 ; x < n ; x++){
+                   arr[x]= thisScans.nextInt();
+                }
+                System.out.println("Array Before Bubble Sort");  
+                for(int i=0; i < arr.length; i++){  
+                        System.out.print(arr[i] + " ");  
+                }  
+                System.out.println();  
+                
+                //Sorting array elements using bubble sort  
+                bubbleSort(arr);
+                
+                //Print the  elements of array after sorting
+                System.out.println("Array After Bubble Sort");  
+                for(int i=0; i < n; i++){  
+                        System.out.print(arr[i] + " ");  
+                }  
+                
+                System.out.println();
+        }
+}
---
+++ src/Practice50/Practice50.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practice50;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jruthie
+ */
+public class Practice50 {
+
+    //The bubble sort method
+        static void bubbleSort(String[] str) {  
+        int n = str.length;  
+        String temp; 
+        //Bubble sort process
+       	for (int j = 0; j < str.length; j++) {
+   	   for (int i = j + 1; i < str.length; i++) {
+		// comparing adjacent strings
+		if (str[i].compareTo(str[j]) < 0) {
+			temp = str[j];
+			str[j] = str[i];
+			str[i] = temp;
+		}
+	   } 
+        }
+  
+  
+    }  
+    public static void main(String[] args) {  
+               //Creating scanner
+               Scanner thisScans = new Scanner(System.in);
+                 
+               //Get the size of array
+                System.out.println("Enter 5 Strings");
+                int n = 5;
+                
+                //Get the elements of an array
+
+                String str[] =new String [n];  
+                for(int x = 0 ; x < n ; x++){
+                   System.out.print("["+(x+1)+"]: ");
+                   str[x]= thisScans.nextLine();
+                }
+                
+                //Sorting array elements using bubble sort  
+                bubbleSort(str);
+                
+                //Print the  elements of array after sorting
+                System.out.println("\nString in sorted order are");  
+                for(int i=0; i < n; i++){  
+                        System.out.print("String ["+(i^+1)+"]: ");
+                        System.out.println(str[i] + " ");  
+                }  
+                
+                System.out.println();
+        }
+}
